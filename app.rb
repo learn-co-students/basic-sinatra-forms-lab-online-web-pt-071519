b2d622d4a04c523d93e59d5c521621af50a534a1
ycorredius <ycorredius@gmail.com>
2019-10-08 09:43:17 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
-require 'sinatra/base'
+
+require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/newteam' do
+        erb :newteam
+    end
 
-
+    post '/team' do
+        team = params
+        @team = team
+        erb :team
+    end
 end
